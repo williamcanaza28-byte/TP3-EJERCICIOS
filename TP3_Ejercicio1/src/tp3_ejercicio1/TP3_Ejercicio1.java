f564dfe2018edb11e0398dfe485e85183cd1e846
williamcanaza28-byte <williamcanaza28@gmail.com>
2026-04-16 22:58:10 -0300

    agrego login

---
+++ TP3_Ejercicio1/src/tp3_ejercicio1/TP3_Ejercicio1.java
@@ -0,0 +1,34 @@
+package tp3_ejercicio1;
+public class TP3_Ejercicio1 {
+    public static void main(String[] args) {
+        int i = 1;
+
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <=10 );
+    }
+}
+
+/*
+package tp3_ejercicio1;
+public class TP3_Ejercicio1 {   
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+} 
+
+package tp3_ejercicio1;
+public class TP3_Ejercicio1 {
+    public static void main(String[] args) {
+       
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
